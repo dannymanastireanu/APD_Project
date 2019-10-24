7bb9d395b373f5ed322197f68c81f82a71959b20
Danny Manastireanu <48862142+dannymanastireanu@users.noreply.github.com>
2019-10-24 20:16:30 +0300

    Add files via upload

---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("APD_PROJECT");
+        Scene scene = new Scene(root, 1680, 720);
+        primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image("icon_graphics.png"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/sample/MainController.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+
+public class MainController {
+    @FXML
+    private Button buttonGenerateSignal;
+    @FXML
+    private MenuItem newFile, loadFile, saveFile, deleteFile, about;
+
+    public void plotSignal() {
+
+    }
+
+    public void onNewFile(ActionEvent event) {
+    }
+
+    public void onLoadFile(ActionEvent event) {
+
+    }
+
+    public void onSaveFile(ActionEvent event) {
+
+    }
+
+    public void onDeleteFile(ActionEvent event) {
+
+    }
+
+    public void onAboutProgram(ActionEvent event) {
+
+    }
+
+}
